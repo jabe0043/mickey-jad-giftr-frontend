c8cc43a347c66e086f2c74b907b3136b674e544e
jung0072 <jung0072@algonquinlive.com>
2023-04-17 13:43:07 -0400

    solve merge error

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -8,13 +8,16 @@ import {
 import { AppHeader, Logo } from "../styled/components";
 
 export default function Header() {
+  const navigate = useNavigate();
   const { personId } = useParams();
   const location = useLocation();
   const pathname = location.pathname;
-  // const loggedOut = pathname === "/"; //url path when user is logged out.
-  const personId = pathname.split('/')[2]; //id of the current selected person
-  const navigate = useNavigate();
 
+  //url path when user is logged out.
+  const loggedOut = pathname === "/";
+  
+  //id of the current selected person
+  // const personId = pathname.split('/')[2];
 
   const logOut = () => {
     sessionStorage.removeItem("UserToken");
@@ -29,44 +32,72 @@ export default function Header() {
     }
   };
   const renderLeftIcon = () => {
-    switch(pathname){
+    switch (pathname) {
       case "/people":
-        return(<i onClick={() => logOut()} className="bi bi-door-open-fill"></i>)
+        return (
+          <i onClick={() => logOut()} className="bi bi-door-open-fill"></i>
+        );
         break;
       case "/people/add":
-        return(<i className="bi bi-arrow-left" onClick={() => navigate("/people")}></i>)
+        return (
+          <i
+            className="bi bi-arrow-left"
+            onClick={() => navigate("/people")}
+          ></i>
+        );
         break;
       case `/gift/${personId}`:
-        return(<i className="bi bi-arrow-left" onClick={() => navigate("/people")}></i>)
+        return (
+          <i
+            className="bi bi-arrow-left"
+            onClick={() => navigate("/people")}
+          ></i>
+        );
         break;
       case `/gift/${personId}/add`:
-        return(<i className="bi bi-arrow-left" onClick={() => navigate(`/gift/${personId}`)}></i>)
+        return (
+          <i
+            className="bi bi-arrow-left"
+            onClick={() => navigate(`/gift/${personId}`)}
+          ></i>
+        );
         break;
       case `/gift/${personId}/edit`:
-        return (<i className="bi bi-arrow-left" onClick={() => navigate(`/gift/${personId}`)}></i>)
+        return (
+          <i
+            className="bi bi-arrow-left"
+            onClick={() => navigate(`/gift/${personId}`)}
+          ></i>
+        );
         break;
-    } 
-  }
+    }
+  };
 
   const renderRightIcon = () => {
-    switch(pathname){
+    switch (pathname) {
       case "/people":
-        return(<i className="bi bi-plus" onClick={() => navigate("/people/add")}></i>)
+        return (
+          <i className="bi bi-plus" onClick={() => navigate("/people/add")}></i>
+        );
         break;
       case "/people/add":
-        return(null)
+        return null;
         break;
       case `/gift/${personId}`:
-        return(<i className="bi bi-plus" onClick={() => navigate(`/gift/${personId}/add`)}></i>)
+        return (
+          <i
+            className="bi bi-plus"
+            onClick={() => navigate(`/gift/${personId}/add`)}
+          ></i>
+        );
         break;
-    } 
-  }
+    }
+  };
 
   return (
     <header>
       <AppHeader className="container">
-      
-      {/*        
+        {/*        
         {renderLeftIcon()}
         <div style={{display:"flex", justifyContent:"center", flexGrow:1}} >
           <Logo onClick={() => navigate("/people")}>GIFT'R</Logo>
